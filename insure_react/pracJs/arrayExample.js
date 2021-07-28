79a729f8667c5a86405879ee71cacc6ee970e75c
skymill2000 <skymill2000@gmail.com>
2021-07-28 11:03:55 +0900

    배열 접근 방법 map, for...of, foreach

--- insure_react/pracJs/arrayExample.js
+++ insure_react/pracJs/arrayExample.js
@@ -9,9 +9,24 @@ let car = {
   },
 };
 
-let cars = ["bmw", "saab", "ford", car, 2];
+let cars = ["bmw", "saab", "ford"];
 
-console.log(cars);
+// console.log(cars[0]);
+// console.log(cars[1]);
+// console.log(cars[2]);
+
+for (let index = 0; index < cars.length; index++) {
+  const car = cars[index];
+  //   console.log(car);
+}
+
+for (car of cars) {
+  console.log(car);
+}
+
+cars.map((car) => {
+  console.log(car);
+});
 
 //String [] names = new Array[3]
 //Arraylist 개념이랑 비슷
