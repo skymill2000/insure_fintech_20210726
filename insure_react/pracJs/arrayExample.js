be627637ba2eb06f6ad9f5982737134b0aba1735
skymill2000 <skymill2000@gmail.com>
2021-07-28 10:44:22 +0900

    array 예제

---
+++ insure_react/pracJs/arrayExample.js
@@ -0,0 +1,17 @@
+let car = {
+  name: "sonata",
+  ph: 500,
+  start: function () {
+    console.log("engine start");
+  },
+  stop: function () {
+    console.log("engine stop");
+  },
+};
+
+let cars = ["bmw", "saab", "ford", car, 2];
+
+console.log(cars);
+
+//String [] names = new Array[3]
+//Arraylist 개념이랑 비슷
--- insure_react/pracJs/funcExample.js
+++ insure_react/pracJs/funcExample.js
@@ -6,7 +6,22 @@ const plus2 = (p1, p2) => {
   return p1 + p2;
 };
 
+const minus = (p1, p2) => {
+  return p1 - p2;
+};
+
+const multi = (p1, p2) => {
+  return p1 * p2;
+};
+
+const div = (p1, p2) => {
+  return p1 / p2;
+};
+
 // - * / 3가지의 연산을 Arrow function 선언해주세요
 
 console.log(plus(1, 2));
 console.log(plus2(1, 2));
+console.log(multi(1, 2));
+console.log(minus(1, 2));
+console.log(div(1, 2));
