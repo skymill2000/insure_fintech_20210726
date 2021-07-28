5728a637fa517c11c374abdaecb894eedbfec815
skymill2000 <skymill2000@gmail.com>
2021-07-28 15:49:05 +0900

    라우터 버그 수정

--- insure_react/insure/src/App.js
+++ insure_react/insure/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ListPage from "./pages/ListPage";
 
 function App() {
-  <Router>
-    <Switch>
-      <Route path="/test" exact>
-        <ListPage></ListPage>
-      </Route>
-    </Switch>
-  </Router>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/test" exact>
+          <ListPage></ListPage>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
