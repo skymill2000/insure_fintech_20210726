dc2ea588d7973a539d08093b811e228a65c6c01a
skymill2000 <skymill2000@gmail.com>
2021-07-28 15:46:06 +0900

    라우터 추가

--- insure_react/insure/src/App.js
+++ insure_react/insure/src/App.js
@@ -1,39 +1,15 @@
-import Header from "./components/Header";
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ListPage from "./pages/ListPage";
 
-const Element = ({ username }) => {
-  return (
-    <>
-      <h1>안녕하세요 {username} 새로운 컴포넌트 입니다.</h1>
-    </>
-  );
-};
-
 function App() {
-  // let userInput = "test";
-  const [userInput, setUserInput] = useState("초기값");
-  const inputChange = (e) => {
-    const { value } = e.target;
-    console.log(value);
-    setUserInput(value);
-  };
-
-  const clickButton = () => {
-    console.log("사용자가 버튼을 클릭했습니다.");
-  };
-
-  return (
-    <div>
-      <Header title="테스트 페이지"></Header>
-      <Element username={"test"}></Element>
-      <b> 사용자 입력 값 : {userInput}</b>
-      <br />
-      <input onChange={inputChange}></input>
-      <button onClick={clickButton}>클릭 이벤트의 바인딩</button>
-      <ListPage></ListPage>
-    </div>
-  );
+  <Router>
+    <Switch>
+      <Route path="/test" exact>
+        <ListPage></ListPage>
+      </Route>
+    </Switch>
+  </Router>;
 }
 
 export default App;
