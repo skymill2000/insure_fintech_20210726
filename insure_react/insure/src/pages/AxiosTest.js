36db8f3dc427a2fc5616cc31bfcccdae904082f4
skymill2000 <skymill2000@gmail.com>
2021-07-29 10:26:29 +0900

    네이버 요청 완료

--- insure_react/insure/src/pages/AxiosTest.js
+++ insure_react/insure/src/pages/AxiosTest.js
@@ -2,11 +2,14 @@ import React from "react";
 import axios from "axios";
 
 const getData = () => {
+  let getData = "데이터 입력입니다.";
   axios
     .get("https://naver.com")
     .then(function (response) {
       // handle success
       console.log(response);
+      getData = response.data;
+      console.log(getData);
     })
     .catch(function (error) {
       // handle error
