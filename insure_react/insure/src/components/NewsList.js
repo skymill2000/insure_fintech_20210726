2655e2c2fb5561a9410a31c175acdb50f4f42afe
skymill2000 <skymill2000@gmail.com>
2021-07-29 11:45:55 +0900

    뉴스 검색 서비스 완성

--- insure_react/insure/src/components/NewsList.js
+++ insure_react/insure/src/components/NewsList.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const NewsList = ({ newsData }) => {
-  return <div>{/* map 뉴스 데이터를 그리기 */}</div>;
+  return (
+    <div>
+      {newsData.map((news, index) => {
+        return (
+          <>
+            <p key={index}>{news.title}</p>
+            <a href={news.url}>기사로 이동하기</a>
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default NewsList;
