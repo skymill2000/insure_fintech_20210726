27a6bc33a24a104e527f5780a8f81e87ef4d1b3b
skymill2000 <skymill2000@gmail.com>
2021-07-29 10:37:44 +0900

    엑시오스 코드 수정

--- insure_react/insure/src/pages/AxiosTest.js
+++ insure_react/insure/src/pages/AxiosTest.js
@@ -3,24 +3,14 @@ import axios from "axios";
 
 const AxiosTest = () => {
   const [htmlGet, setHtmlGet] = useState("initialState");
-
   const getData = () => {
     let getData = "데이터 입력입니다.";
-    axios
-      .get("https://naver.com")
-      .then(function (response) {
-        // handle success
-        console.log(response);
-        getData = response.data;
-        setHtmlGet(getData);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
+    axios.get("https://naver.com").then(function (response) {
+      // handle success
+      console.log(response);
+      getData = response.data;
+      // setHtmlGet(getData);
+    });
   };
   getData();
   return <div>{htmlGet}</div>;
