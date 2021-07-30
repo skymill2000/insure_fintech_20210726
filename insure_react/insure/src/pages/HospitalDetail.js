cd9c2d26091af7be0ae764c99134e10dbe769c97
skymill2000 <skymill2000@gmail.com>
2021-07-30 10:26:13 +0900

    useEffect 첫 ㅅ ㅏ용

--- insure_react/insure/src/pages/HospitalDetail.js
+++ insure_react/insure/src/pages/HospitalDetail.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import { useLocation } from "react-router-dom";
 
 const HospitalDetail = () => {
   const { search } = useLocation();
-  console.log(search);
   const { ykiho } = queryString.parse(search);
-  console.log(ykiho);
+  useEffect(() => {
+    getHospitalDetailData();
+  }, []);
+
+  const getHospitalDetailData = () => {
+    console.log("요청을 만들어 주세요");
+  };
 
   //useEffect 사용해서  <-- 검색을해서 작성을 부탁드립니다.
   //시작하자마자 api 요청을 만들기
--- insure_react/pracJs/caecerTest.js
+++ insure_react/pracJs/caecerTest.js
@@ -1,2 +1,3 @@
 let caecer = require("./caecer");
+console.log(caecer.encrypt(20, "show me the money"));
 console.log(caecer.decrypt(5, "kl krz duh brx l dp ilqh"));
