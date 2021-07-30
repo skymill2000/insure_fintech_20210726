76a0e72323779ee8f3680efb57e3e12544970cc0
skymill2000 <skymill2000@gmail.com>
2021-07-30 10:50:50 +0900

    병원 디테일 조회

--- insure_react/insure/src/pages/HospitalDetail.js
+++ insure_react/insure/src/pages/HospitalDetail.js
@@ -1,22 +1,56 @@
 import React, { useState, useEffect } from "react";
 import queryString from "query-string";
 import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 const HospitalDetail = () => {
   const { search } = useLocation();
+  const [result, setResult] = useState();
   const { ykiho } = queryString.parse(search);
   useEffect(() => {
     getHospitalDetailData();
   }, []);
 
   const getHospitalDetailData = () => {
-    console.log("요청을 만들어 주세요");
+    const option = {
+      method: "GET",
+      url: "/B551182/hospDiagInfoService1/getClinicTop5List1",
+      headers: {},
+      params: {
+        ServiceKey:
+          "uiu3ZzNzDB04UbxOtOL1atH04WOtxB5WSKkPbaCASVHbwgcsIPwHA5Qp6xOmSe6fzCnUVifZcfTXDkgNegv4qQ==",
+        pageNo: "1",
+        numOfRows: "10",
+        ykiho: ykiho,
+      },
+    };
+    axios(option).then((response) => {
+      console.log(response.data.response.body.items.item);
+      setResult(response.data.response.body.items.item);
+    });
   };
 
   //useEffect 사용해서  <-- 검색을해서 작성을 부탁드립니다.
   //시작하자마자 api 요청을 만들기
 
-  return <div></div>;
+  return (
+    <div>
+      {result !== undefined && (
+        <>
+          <p>{result.mfrnIntrsIlnsCdNm1}</p>
+          <p>{result.mfrnIntrsIlnsCdNm2}</p>
+          <p>{result.mfrnIntrsIlnsCdNm3}</p>
+          <p>{result.mfrnIntrsIlnsCdNm4}</p>
+          <p>{result.mfrnIntrsIlnsCdNm5}</p>
+        </>
+      )}
+      {result === undefined && (
+        <>
+          <p>현재 등록된 데이터가 없습니다</p>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default HospitalDetail;
